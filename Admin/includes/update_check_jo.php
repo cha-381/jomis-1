a45b0bc121c60c9c2488a099203ad5d5e06482e1
Charity <madridcharity@gmail.com>
2022-04-27 08:46:52 +0800

    Create update_check_jo.php

---
+++ Admin/includes/update_check_jo.php
@@ -0,0 +1,33 @@
+<?php
+    
+ include('../config/db_config.php');
+   if (isset($_POST['update'])) {
+
+        $job_num = $_POST['id'];
+        $job_no = $_POST['Remarks'];
+        $no = $_POST['id_no'];  
+  
+
+     $sql ="UPDATE schedule SET 
+            Remarks                   = '$job_no'
+   
+        WHERE   id = '$job_num' ";
+
+ 
+    if($con->query($sql)){
+             $alert_msg .= '<div class="alert alert-success alert-dismissible"><i class="icon fa fa-check"></i>Successfully Updated</div>';
+
+    $btnAdd = 'disabled';
+    $btnStatus = 'disabled';
+     $btnNew = 'enabled';
+    }
+        else{
+            $_SESSION['error'] = $con->error;
+        }
+    }
+    else{
+        $_SESSION['error'] = 'Fill up edit form first';
+    }
+
+  
+?>
