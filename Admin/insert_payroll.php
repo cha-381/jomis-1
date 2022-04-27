35b0e4ebe300f705f99d3c1ef0bd08c25d7ce523
Charity <madridcharity@gmail.com>
2022-04-27 08:47:26 +0800

    Update insert_payroll.php

--- Admin/insert_payroll.php
+++ Admin/insert_payroll.php
@@ -18,8 +18,11 @@
     $id_number = $_POST['id_number'];
     $id_num = $_POST['id_num'];
     $status = $_POST['status'];
+    $dept = $_POST['dept'];
+    $period = $_POST['period'];
  
     $amount = $_POST['amount'];
+    $subject = "Done";
       
     $insert_schedule_sql  = "INSERT INTO payroll SET 
         JobOrderNo               = :jo_no,
@@ -32,6 +35,8 @@
         id_no                    = :id_number,
         id_num                    = :id_num,
         status                   = :status,
+        Department               = :dept,
+        PeriodCovered            = :period,
       
         Charges                  = :proj_charges
         ";
@@ -48,6 +53,8 @@
         ':id_number'         => $id_number,
         ':id_num'            => $id_num,
         ':status'            => $status,
+        ':dept'              => $dept,
+        ':period'            => $period,
         ':proj_charges'      => $proj_charges
       ]);
 
@@ -56,8 +63,8 @@
 
 
 $sql ="UPDATE `project` SET Balance   = '$amount',
-                    Actual   = '$amount'
-        WHERE   objid               = '$id_num'  
+               Actual       =        '$amount'
+        WHERE   objid       =        '$id_num'  
      ";
 
 $sql1 = "UPDATE `createjo` SET status   = '$status',
@@ -70,7 +77,13 @@ $sql2 = "UPDATE `schedule` SET PayrollNo   = '$pay_no'
         WHERE   JobOrderNo               = '$jo_no'  
      ";
 
-if($con->query($sql) And $con->query($sql1) And $con->query($sql2)){
+$sql3 = "UPDATE `createjo` SET 
+                Subject                 = '$subject'
+        WHERE   objid  < '$id_number'  AND Charges = '$proj_charges' AND objid_pro = '$id_num'
+     ";
+
+
+if($con->query($sql) And $con->query($sql1) And $con->query($sql2) And $con->query($sql3)){
      
      $alert_msg .= ' 
           <div class="alert alert-success alert-dismissible">
