c21e08cb5b4362557c619031118f9c022351dce8
Charity <madridcharity@gmail.com>
2022-04-27 08:46:55 +0800

    Update update_edit_jo.php

--- Admin/includes/update_edit_jo.php
+++ Admin/includes/update_edit_jo.php
@@ -3,7 +3,7 @@
  include('../config/db_config.php');
    if (isset($_POST['update'])) {
 
-      
+        $job_num = $_POST['objid'];
         $job_no = $_POST['JobOrderNo'];  
         $jo_amount = $_POST ['Amount'];
         $job_prev = $_POST['PreviousBalance'];
@@ -11,30 +11,42 @@
         $job_pro = $_POST['objid_pro'];
         $job_code = $_POST['code'];
         $job_laborers = $_POST['job_laborers'];
-    
-
+        $job_bal = $_POST['Balance'];
+        $job_balc = $_POST['Bal'];
+        $job_subject = $_POST['Subject'];
+        $job_sub = "Modified";
+      
     
     $sql ="UPDATE createjo SET 
             Amount                 = '$jo_amount',
-            PreviousBalance        = '$job_prev',
+            objid                  = '$job_num',
             Laborers               = '$job_laborers',
-            Charges                 = '$job_charge'
+            Charges                 = '$job_charge',
+            Subject                 = '$job_subject',
+            Bal                    = '$job_balc',
+            PreviousBalance        = '$job_prev'
 
         WHERE   JobOrderNo = '$job_no' 
      ";
 
      $sql1 ="UPDATE schedule SET 
             noLaborers               = '$job_laborers',
-            Amount                 = '$jo_amount'
+            Amount                   = '$jo_amount'
    
         WHERE   JobOrderNo = '$job_no' ";
 
      $sql2 ="UPDATE project SET 
-            Balance                 = '$job_prev'
+            Balance                 = '$job_bal'
    
         WHERE  objid = '$job_pro' and code = '$job_code'
              ";
-    if($con->query($sql) && $con->query($sql1) && $con->query($sql2)){
+
+     $sql3 ="UPDATE createjo SET 
+            Subject                 = '$job_sub'
+
+        WHERE   objid > '$job_num' AND Charges = '$job_charge' AND objid_pro = '$job_pro' ";
+     
+    if($con->query($sql) && $con->query($sql1) && $con->query($sql2) && $con->query($sql3)){
              $alert_msg .= '<div class="alert alert-success alert-dismissible"><i class="icon fa fa-check"></i>Successfully Updated</div>';
 
     $btnAdd = 'disabled';
